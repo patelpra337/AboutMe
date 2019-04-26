b36933fcf92bd32cd12f7ed8d669f6c32137962d
patelpra337 <patelpra337@gmail.com>
2019-04-25 21:25:00 -0500

    Remove print statement

--- AboutMe (iOSPT1)/ViewController.swift
+++ AboutMe (iOSPT1)/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hi my name is Pravin Patel")
-        print("Am new to learning Swift")
+
     }
 
 
