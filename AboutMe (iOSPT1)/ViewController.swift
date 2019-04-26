1e21a4c477dd4ed1087a951f02ac1e7eec8d731f
patelpra337 <patelpra337@gmail.com>
2019-04-25 21:41:25 -0500

    Added IBoutlets

--- AboutMe (iOSPT1)/ViewController.swift
+++ AboutMe (iOSPT1)/ViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var hobbiesLabel: UILabel!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
     }
